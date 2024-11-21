85ec88635d1532614490ad124783bd32c3d9b768
Aaratichauhan <86142976+Aaratichauhan@users.noreply.github.com>
2024-11-21 23:53:08 +0530

    Update index.php

--- triplet_in_the_array/index.php
+++ triplet_in_the_array/index.php
@@ -1,27 +1,32 @@
-<?php
-function findTriplet($arr, $sum) {
+function findTriplet($arr, $val) {
     $n = count($arr);
+    sort($arr);
 
     for ($i = 0; $i < $n - 2; $i++) {
-        for ($j = $i + 1; $j < $n - 1; $j++) {
-            for ($k = $j + 1; $k < $n; $k++) {
-                if ($arr[$i] + $arr[$j] + $arr[$k] == $sum) {
-                    echo "{" . $arr[$i] . ", " . $arr[$j] . ", " . $arr[$k] . "}\n";
-                    return true;
-                }
+        $left = $i + 1;
+        $right = $n - 1;
+
+        while ($left < $right) {
+            $current_sum = $arr[$i] + $arr[$left] + $arr[$right];
+            if ($current_sum == $val) {
+                return array($arr[$i], $arr[$left], $arr[$right]);
+            } elseif ($current_sum < $val) {
+                $left++;
+            } else {
+                $right--;
             }
         }
     }
-
     return false;
 }
 
-// Sample Input
-$a = array(12, 3, 4, 1, 6, 9);
+// Sample input
+$arr = array(12, 3, 4, 1, 6, 9);
 $val = 24;
 
-// Find and print the triplet
-if (!findTriplet($a, $val)) {
-    echo "No triplet found\n";
+$triplet = findTriplet($arr, $val);
+if ($triplet) {
+    echo "Triplet found: {" . implode(", ", $triplet) . "}";
+} else {
+    echo "No triplet found";
 }
-?>
